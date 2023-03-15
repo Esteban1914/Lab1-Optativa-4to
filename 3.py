6055b4e5cc80e434448316548ac87bc13e63c9ea
Esteban <acevedoesteban999@gmail.com>
2023-03-15 01:25:38 -0400

    -

--- 3.py
+++ 3.py
@@ -2,7 +2,7 @@ from datetime import datetime
 if __name__=='__main__':
     my_list=[
         "N:Amanda,CI:03062224105,A:Industrial",
-        "N:Esteban,CI:00071965760,A:Automatica",
+        "N:Alberto,CI:03082165760,A:Mecanica",
         "N:Maria,CI:97022324402,A:Civil",
         "N:Melisa,CI:99052234145,A:Arquitectura",
         "N:Claudia,CI:03082124902,A:Quimica",
@@ -10,9 +10,9 @@ if __name__=='__main__':
     def myFunc(e):
         try:
             if e[e.find("CI:")+3 : e.find("CI:")+5] >  datetime.today().year.__str__()[2:]:
-                return '1'.join(e[e.find("CI:")+3 : e.find("CI:")+3])
+                return '1'+ e[e.find("CI:")+3 : e.find("CI:")+9]
             else:
-                return "2".join(e[e.find("CI:")+3 : e.find("CI:")+9])
+                return '2'+e[e.find("CI:")+3 : e.find("CI:")+9]
         except:
             print("Ha ocurrido un error")
             return None
